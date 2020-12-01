1bf3c801fbbafeccad3553b3130f8a52825f3a85
Alex Erdei <mralexerdei@yahoo.co.uk>
2020-12-01 12:53:03 +0000

    make GeneralInformation functional component

--- src/components/generalInformation.jsx
+++ src/components/generalInformation.jsx
@@ -1,121 +1,108 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { PencilSquare } from "react-bootstrap-icons";
 
-class GeneralInformation extends Component {
-  state = {
-    isEditing: this.props.isEditing,
+const GeneralInformation = (props) => {
+  const [isEditing, setIsEditing] = useState(props.isEditing);
+
+  const handleClick = (event) => {
+    setIsEditing(true);
   };
 
-  handleClick = (event) => {
-    const isEditing = true;
-    this.setState({ isEditing });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    const isEditing = false;
-    this.setState({ isEditing });
+    setIsEditing(false);
   };
 
-  render() {
-    const form = (
-      <div className="container">
-        <div className="row border border-dark">
-          <div className="col-md-12 m-4">
-            <form>
-              <div className="row w-100">
-                <div className="form-group col-md-4">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="name"
-                    id="name"
-                    value={this.props.person.name}
-                    onChange={this.props.onChange}
-                    required
-                  />
-                </div>
-                <div className="form-group col-md-4">
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    name="email"
-                    id="email"
-                    value={this.props.person.email}
-                    onChange={this.props.onChange}
-                    required
-                    pattern
-                  />
-                </div>
-                <div className="form-group col-md-4">
-                  <label htmlFor="phone">Phone Number</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    name="phone"
-                    id="phone"
-                    value={this.props.person.phone}
-                    onChange={this.props.onChange}
-                  />
-                </div>
+  const form = (
+    <div className="container">
+      <div className="row border border-dark">
+        <div className="col-md-12 m-4">
+          <form onSubmit={handleSubmit}>
+            <div className="row w-100">
+              <div className="form-group col-md-4">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="name"
+                  id="name"
+                  value={props.person.name}
+                  onChange={props.onChange}
+                  required
+                />
               </div>
-              <div className="row w-100">
-                <div className="form-group col-md-12">
-                  <label htmlFor="personalInformation">
-                    Personal Information
-                  </label>
-                  <textarea
-                    className="form-control"
-                    name="personalInformation"
-                    id="personalInformation"
-                    cols="30"
-                    rows="10"
-                    value={this.props.person.personalInformation}
-                    onChange={this.props.onChange}
-                    required
-                  ></textarea>
-                </div>
+              <div className="form-group col-md-4">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  name="email"
+                  id="email"
+                  value={props.person.email}
+                  onChange={props.onChange}
+                  required
+                  pattern
+                />
               </div>
-              <div className="row">
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-sm m-3"
-                  onSubmit={this.handleSubmit}
-                >
-                  Submit
-                </button>
+              <div className="form-group col-md-4">
+                <label htmlFor="phone">Phone Number</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="phone"
+                  id="phone"
+                  value={props.person.phone}
+                  onChange={props.onChange}
+                />
               </div>
-            </form>
-          </div>
+            </div>
+            <div className="row w-100">
+              <div className="form-group col-md-12">
+                <label htmlFor="personalInformation">
+                  Personal Information
+                </label>
+                <textarea
+                  className="form-control"
+                  name="personalInformation"
+                  id="personalInformation"
+                  cols="30"
+                  rows="10"
+                  value={props.person.personalInformation}
+                  onChange={props.onChange}
+                  required
+                ></textarea>
+              </div>
+            </div>
+            <div className="row">
+              <button type="submit" className="btn btn-primary btn-sm m-3">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    );
+    </div>
+  );
 
-    const page = (
-      <div className="container">
-        <h1 className="text-center">{this.props.person.name}</h1>
-        <div className="text-center">{this.props.person.email}</div>
-        <div className="text-center">{this.props.person.phone}</div>
-        <br />
-        <div className="row border border-dark">
-          <h4 className="text-left bg-dark text-light w-100">
-            Personal Information
-          </h4>
-          <p>{this.props.person.personalInformation}</p>
-        </div>
-        <button
-          className="btn btn-secondary btn-sm m-2"
-          onClick={this.handleClick}
-        >
-          <PencilSquare />
-        </button>
+  const page = (
+    <div className="container">
+      <h1 className="text-center">{props.person.name}</h1>
+      <div className="text-center">{props.person.email}</div>
+      <div className="text-center">{props.person.phone}</div>
+      <br />
+      <div className="row border border-dark">
+        <h4 className="text-left bg-dark text-light w-100">
+          Personal Information
+        </h4>
+        <p>{props.person.personalInformation}</p>
       </div>
-    );
+      <button className="btn btn-secondary btn-sm m-2" onClick={handleClick}>
+        <PencilSquare />
+      </button>
+    </div>
+  );
 
-    return (this.state.isEditing && form) || page;
-  }
-}
+  return (isEditing && form) || page;
+};
 
 export default GeneralInformation;
